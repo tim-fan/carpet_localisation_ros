47699cc0558982b53567f88387b383075294e5cb
tim-fan <tim.fanselow@gmail.com>
2021-01-09 23:31:53 +1300

    Add particle publishing in RViz, motion thresholds for particle updates

--- scripts/carpet_localiser.py
+++ scripts/carpet_localiser.py
@@ -1,45 +1,103 @@
 #!/usr/bin/env python
-from typing import Tuple
+from typing import Tuple, Optional
 import pickle
 import numpy as np
 import rospy
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Quaternion, PoseArray
+from geometry_msgs.msg import Pose as PoseMsg
 from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge
 import message_filters
-from tf.transformations import euler_from_quaternion
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from carpet_color_classification import CarpetColorClassifier
 from cbl_particle_filter.filter import CarpetBasedParticleFilter, Pose, OdomMeasurement, ColorMeasurement
 from cbl_particle_filter.carpet_map import load_map_from_png
 
-def odom_msg_to_measurement(odom_msg:Odometry) -> OdomMeasurement:
+
+def yaw_from_quaternion_msg(q: Quaternion) -> float:
+    _, _, yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
+    return yaw
+
+
+def quaternion_msg_from_yaw(yaw: float) -> Quaternion:
+    q_list = quaternion_from_euler(0, 0, yaw)
+    q = Quaternion(
+        x=q_list[0],
+        y=q_list[1],
+        z=q_list[2],
+        w=q_list[3],
+    )
+    return q
+
+
+def odom_msg_to_measurement(odom_msg: Odometry) -> OdomMeasurement:
     """
     Convert ROS odom to the cbl representation
     """
-    q = odom_msg.pose.pose.orientation
-    _, _, heading = euler_from_quaternion([q.x, q.y, q.z, q.w])
+    heading = yaw_from_quaternion_msg(odom_msg.pose.pose.orientation)
     return OdomMeasurement(
         odom_msg.pose.pose.position.x,
         odom_msg.pose.pose.position.y,
         heading,
     )
 
-def compute_odom_delta(current_odom:OdomMeasurement, previous_odom:OdomMeasurement) -> OdomMeasurement:
+
+def odom_msg_to_pose(odom_msg: Odometry) -> Pose:
+    """
+    Convert ROS odom to the cbl pose representation
+    """
+    heading = yaw_from_quaternion_msg(odom_msg.pose.pose.orientation)
+    return Pose(
+        odom_msg.pose.pose.position.x,
+        odom_msg.pose.pose.position.y,
+        heading,
+    )
+
+
+def pose_to_pose_msg(pose: Pose) -> PoseMsg:
+    """
+    convert cbl pose to ROS pose message
+    """
+    pose_msg = PoseMsg()
+    pose_msg.position.x = pose.x
+    pose_msg.position.y = pose.y
+    pose_msg.orientation = quaternion_msg_from_yaw(pose.heading)
+    return pose_msg
+
+
+def wrap_plus_minus_pi(angle):
+    return np.mod(angle + np.pi, 2 * np.pi) - np.pi
+
+
+def compute_odom_delta(current_odom: OdomMeasurement,
+                       previous_odom: OdomMeasurement) -> OdomMeasurement:
     """
     return pose delta from prev to current, in prev frame
     """
     if previous_odom is None:
-        return OdomMeasurement(0,0,0)
+        return OdomMeasurement(0, 0, 0)
 
     dx_global = current_odom.dx - previous_odom.dx
     dy_global = current_odom.dy - previous_odom.dy
     prev_heading = previous_odom.dheading
 
-    return OdomMeasurement(
-        dx =  dx_global * np.cos(prev_heading) + dy_global * np.sin(prev_heading),
-        dy = -dx_global * np.sin(prev_heading) + dy_global * np.cos(prev_heading),
-        dheading= current_odom.dheading - previous_odom.dheading
-    )
+    return OdomMeasurement(dx=dx_global * np.cos(prev_heading) +
+                           dy_global * np.sin(prev_heading),
+                           dy=-dx_global * np.sin(prev_heading) +
+                           dy_global * np.cos(prev_heading),
+                           dheading=wrap_plus_minus_pi(current_odom.dheading -
+                                                       previous_odom.dheading))
+
+
+def particles_to_pose_array(particles: np.ndarray) -> PoseArray:
+    """
+    Convert particle filter particles into a ROS pose array message
+    """
+    def particle_to_pose(particle: np.array) -> Pose:
+        return pose_to_pose_msg(Pose(*particle))
+
+    return PoseArray(poses=[particle_to_pose(p) for p in particles])
 
 
 class CarpetLocaliser():
@@ -47,12 +105,14 @@ class CarpetLocaliser():
     Interface between incoming ROS messages (odom and camera image) and
     carpet based particle filter.
     """
-    def __init__(
-            self,
-            map_png_file:str,
-            map_cell_size:float,
-            classifier_param_file:str,
-            log_inputs=False):
+    def __init__(self,
+                 map_png_file: str,
+                 map_cell_size: float,
+                 classifier_param_file: str,
+                 log_inputs=False):
+
+        self.update_distance_threshold = 0.2  # m
+        self.update_rotation_threshold = 0.1  # rad
 
         self.color_classifier = CarpetColorClassifier(classifier_param_file)
         self.log_inputs = log_inputs
@@ -63,38 +123,85 @@ class CarpetLocaliser():
         self.cv_bridge = CvBridge()
         self.previous_odom = None
 
+        self.pose_pub = rospy.Publisher("current_pose",
+                                        Odometry,
+                                        queue_size=10)
+        self.particle_pub = rospy.Publisher(
+            "particlecloud",
+            PoseArray,
+            queue_size=10,
+        )
+
     def __del__(self):
         if self.log_inputs:
             log_path = "/tmp/localiser_input_log.pickle"
-            self.particle_filter.write_input_log(log_path)  
+            self.particle_filter.write_input_log(log_path)
             rospy.loginfo(f"Saved localiser input log to {log_path}")
 
-
-    def localisation_update(self, odom_msg:Odometry, img_msg:Image):
+    def localisation_update(self,
+                            odom_msg: Odometry,
+                            img_msg: Image,
+                            ground_truth_state: Optional[Odometry] = None):
         """
         Update localisation based on given odom,image pair
+        Can optionally provide ground truth for logging purposes
         """
-        rospy.loginfo("callback")
-
-        # determine detected color
-        color_measurement, color_name = self._classify_image_color(img_msg)
-        rospy.loginfo(f"detected color: {color_name}")
 
         # determine odom delta since last update
         current_odom = odom_msg_to_measurement(odom_msg)
         odom_delta = compute_odom_delta(current_odom, self.previous_odom)
-        rospy.loginfo(f"odom: {odom_delta}")
-        self.previous_odom = current_odom
 
-        self.particle_filter.update(odom_delta, color_measurement)
+        # perform particle filter update, only if robot has travelled beyond
+        # a certain distance since the previous update
+        distance_since_last_update = np.sqrt(odom_delta.dx**2 +
+                                             odom_delta.dy**2)
+        rotation_since_last_update = np.abs(odom_delta.dheading)
 
+        if distance_since_last_update > self.update_distance_threshold or \
+           rotation_since_last_update > self.update_rotation_threshold or \
+           self.previous_odom is None:
 
+            rospy.loginfo(f"odom: {odom_delta}")
 
-    def _classify_image_color(self, img_msg:Image) -> Tuple[ColorMeasurement, str]:
+            # determine detected color
+            color_measurement, color_name = self._classify_image_color(img_msg)
+            rospy.loginfo(f"detected color: {color_name}")
+
+            # get ground truth pose if provided
+            if ground_truth_state:
+                ground_truth_pose = odom_msg_to_pose(ground_truth_state)
+                rospy.loginfo(f"ground_truth: {ground_truth_pose}")
+            else:
+                ground_truth_pose = None
+
+            # perform update
+            self.particle_filter.update(odom_delta, color_measurement,
+                                        ground_truth_pose)
+
+            # create and publish odom msg representing current pose
+            current_pose = Odometry()
+            current_pose.pose.pose = pose_to_pose_msg(
+                self.particle_filter.get_current_pose())
+            current_pose.header.frame_id = "map"
+            current_pose.header.stamp = img_msg.header.stamp
+            current_pose.twist = odom_msg.twist
+            self.pose_pub.publish(current_pose)
+
+            # also publish current particles
+            pose_array = particles_to_pose_array(
+                self.particle_filter.get_particles())
+            pose_array.header = current_pose.header
+            self.particle_pub.publish(pose_array)
+
+            self.previous_odom = current_odom
+
+    def _classify_image_color(self,
+                              img_msg: Image) -> Tuple[ColorMeasurement, str]:
         """
         invoke classifier on given image, returning color index and name (string)
         """
-        cv_image = self.cv_bridge.imgmsg_to_cv2(img_msg, desired_encoding='bgr8')
+        cv_image = self.cv_bridge.imgmsg_to_cv2(img_msg,
+                                                desired_encoding='bgr8')
         color_index, color_name = self.color_classifier.classify(cv_image)
 
         # set index to None if classification failed
@@ -115,20 +222,32 @@ def run_localisation():
     # as a pickle file, for later offline playback
     log_inputs = rospy.get_param("~log_inputs", default=False)
 
-    localiser = CarpetLocaliser(map_png_file, map_cell_size, classifier_param_file, log_inputs)
+    # param 'subscribe_ground_truth': set true to subscribe to ground truth pose
+    # (expected use-case = use with 'log_inputs' to log gazebo ground truth pose, for testing)
+    subscribe_ground_truth = rospy.get_param("~subscribe_ground_truth",
+                                             default=False)
+
+    localiser = CarpetLocaliser(map_png_file, map_cell_size,
+                                classifier_param_file, log_inputs)
 
     odom_sub = message_filters.Subscriber('odom', Odometry)
     image_sub = message_filters.Subscriber('image', Image)
+    subs = [odom_sub, image_sub]
 
-    time_synchronizer = message_filters.ApproximateTimeSynchronizer([odom_sub, image_sub], 10, 0.2)
+    if subscribe_ground_truth:
+        ground_truth_sub = message_filters.Subscriber('ground_truth/state',
+                                                      Odometry)
+        subs.append(ground_truth_sub)
+
+    time_synchronizer = message_filters.ApproximateTimeSynchronizer(
+        subs, 10, 0.2)
     time_synchronizer.registerCallback(localiser.localisation_update)
 
-
-    def img_callback(img_msg:Image):
+    def img_callback(img_msg: Image):
         cv_image = cv_bridge.imgmsg_to_cv2(img_msg, desired_encoding='bgr8')
         color_index, color_name = classifier.classify(cv_image)
         rospy.loginfo(f"detected color: {color_name}")
-        
+
     try:
         rospy.spin()
     except KeyboardInterrupt:
@@ -136,7 +255,7 @@ def run_localisation():
 
     del localiser
     print("DONE!")
-    
+
 
 if __name__ == '__main__':
     run_localisation()
